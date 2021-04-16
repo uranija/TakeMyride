150345de0347d78169468ca06fb023192cc829c1
uranija <uranijaa@gmail>
2021-04-16 13:09:42 +0300

    second commit

---
+++ app/src/main/java/com/apps/android/takemyride/PayPalConfig.java
@@ -0,0 +1,8 @@
+package com.apps.android.takemyride;
+
+public class PayPalConfig {
+
+    public final static String PAYPAL_CLIENT_ID="AU5Vw97itGgZ3BnU4LzppnKKstbtTbzq1nFiM5Uk8eEz3qjKHKXdSK7du0f4BImKbqFzUphw8hw_cu_o";
+
+    }
+
